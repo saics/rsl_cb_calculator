e4e869a907c7c5344f1a5376563f37726fc43cb5
Simun Saic <simunsaic01@gmail.com>
2023-03-24 21:14:33 +0100

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App(){
+  return (
+    <div className="App">
+      <header className='App-header'>
+        <h1>Clan Boss Calculator</h1>
+      </header>
+      <main>
+        {/* Komponente */}
+      </main>
+    </div>
+  )
+}
+
+export default App;
